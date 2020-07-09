a73e00e78c17d5a21ef3b91c0f9e47bffb0f2423
Sondre Steinsland Hegdal <imaltont@users.noreply.github.com>
2020-07-09 10:07:29 +0200

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
 use config_struct::{Error, StructOptions};
-
+//another test
 fn main() -> Result<(), Error> {
     config_struct::create_struct("twitch_settings.json", "src/config.rs", &StructOptions::default())
 }
